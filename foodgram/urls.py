ac43fa642d42c26fc6b48dd0c16915259e95c346
vamotest <vamotest@gmail.com>
2021-03-14 16:02:54 +0300

    Add flatpages urls

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path, include
-
+from django.contrib.flatpages import views
+from django.urls import include, path
 
 urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('auth/', include('users.urls')),
+]
+
+urlpatterns += [
+    path('about/', include('django.contrib.flatpages.urls')),
+    path('author/', views.flatpage, {'url': '/author/'}, name='author'),
+    path('tech/', views.flatpage, {'url': '/tech/'}, name='tech'),
 ]
