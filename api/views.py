2573e4e59558827f941e6a40f71c2eadf56559a0
vamotest <vamotest@gmail.com>
2021-03-14 19:12:35 +0300

    Add IngredientViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import filters, mixins, viewsets
 
-# Create your views here.
+from recipes.models import Ingredient
+
+from .serializers import IngredientSerializer
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('^title',)
