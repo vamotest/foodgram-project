505bee4bc569ba8c884755969d859b755436d7e3
vamotest <vamotest@gmail.com>
2021-03-14 18:10:03 +0300

    Add pagitor

---
+++ recipes/utils.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.core.paginator import Paginator
+
+
+def get_paginated_view(request, some_list):
+    paginator = Paginator(some_list, settings.PAGINATION_PAGE_SIZE)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return page, paginator
