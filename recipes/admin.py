4048d472d09e2e99307ae5d9c09dd77d5c58a4d4
vamotest <vamotest@gmail.com>
2021-03-14 18:03:17 +0300

    Add admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.db.models import Count
 
-# Register your models here.
+from .models import Ingredient, Recipe, RecipeIngredient, Tag
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    min_num = 1
+    extra = 0
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (RecipeIngredientInline, )
+    list_display = (
+        'id', 'title', 'author', 'slug', 'cooking_time',
+        'get_favorite_count', 'pub_date'
+    )
+    list_filter = ('author', 'tags__title')
+    search_fields = ('title', 'author__username')
+    autocomplete_fields = ('author', )
+    ordering = ('-pub_date', )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.annotate(favorite_count=Count('favored_by'))
+    
+    @staticmethod
+    def get_favorite_count(obj):
+        return obj.favorite_count
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('title', 'dimension')
+    search_fields = ('^title', )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title', 'color', 'display_name')
+    list_filter = ('title', )
