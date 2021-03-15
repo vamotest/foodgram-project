f0192a45164448cf98e937192e5270d3b3376534
vamotest <vamotest@gmail.com>
2021-03-15 19:43:03 +0300

    Add CreateDestroyViewSet

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import filters, mixins, viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, status, viewsets
+from rest_framework.response import Response
 
 from recipes.models import Ingredient
 
@@ -10,3 +12,27 @@ class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = IngredientSerializer
     filter_backends = (filters.SearchFilter, )
     search_fields = ('^title',)
+
+
+class CreateDestroyViewSet(mixins.CreateModelMixin,
+                           mixins.DestroyModelMixin,
+                           viewsets.GenericViewSet):
+
+    def get_object(self, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        lookup_url = self.lookup_url_kwarg or self.lookup_field
+        kwargs_filter = {
+            self.lookup_field:
+                self.kwargs[lookup_url], **kwargs
+        }
+
+        obj = get_object_or_404(queryset, **kwargs_filter)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object(user=self.request.user)
+        success = instance.delete()
+        context = {'success': bool(success)}
+        return Response(context, status=status.HTTP_200_OK)
