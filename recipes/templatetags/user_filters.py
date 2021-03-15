a5d4525c743f24e2aa15569879c563aca49ce912
vamotest <vamotest@gmail.com>
2021-03-15 20:12:58 +0300

    Add Favorite filter

--- recipes/templatetags/user_filters.py
+++ recipes/templatetags/user_filters.py
@@ -1,7 +1,7 @@
 from django import template
 from django.contrib.auth import get_user_model
 
-from api.models import Subscription
+from api.models import Favorite, Subscription
 
 register = template.Library()
 User = get_user_model()
@@ -15,3 +15,8 @@ def get_full_name_or_username(user):
 @register.filter
 def is_subscribed_to(user, author):
     return Subscription.objects.filter(user=user, author=author).exists()
+
+
+@register.filter
+def is_favored_by(recipe, user):
+    return Favorite.objects.filter(recipe=recipe, user=user).exists()
