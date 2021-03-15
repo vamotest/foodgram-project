cc8042dd42287a39a05c0cf62e757f5c420aa1de
vamotest <vamotest@gmail.com>
2021-03-15 20:28:27 +0300

    Add template recipe_view

--- recipes/templatetags/user_filters.py
+++ recipes/templatetags/user_filters.py
@@ -20,3 +20,15 @@ def is_subscribed_to(user, author):
 @register.filter
 def is_favored_by(recipe, user):
     return Favorite.objects.filter(recipe=recipe, user=user).exists()
+
+
+@register.filter
+def conjoin(number, args):
+    args = [arg.strip() for arg in args.split(',')]
+    last_digit = int(number) % 10
+    if last_digit == 1:
+        return f'{number} {args[0]}'
+    elif 1 < last_digit < 5:
+        return f'{number} {args[1]}'
+    elif last_digit > 4 or last_digit == 0:
+        return f'{number} {args[2]}'
