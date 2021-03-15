461cac3ade3abbd19bfd83ad6e1187e1e2ba8e03
vamotest <vamotest@gmail.com>
2021-03-15 20:12:22 +0300

    Add Subscription filter

--- recipes/templatetags/user_filters.py
+++ recipes/templatetags/user_filters.py
@@ -1,6 +1,8 @@
 from django import template
 from django.contrib.auth import get_user_model
 
+from api.models import Subscription
+
 register = template.Library()
 User = get_user_model()
 
@@ -8,3 +10,8 @@ User = get_user_model()
 @register.filter
 def get_full_name_or_username(user):
     return user.get_full_name() or user.username
+
+
+@register.filter
+def is_subscribed_to(user, author):
+    return Subscription.objects.filter(user=user, author=author).exists()
