2ee6dc257c0fe8027d81202726cb445f98e9536e
vamotest <vamotest@gmail.com>
2021-03-15 19:45:16 +0300

    Add CustomModelSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -7,3 +7,9 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('title', 'dimension')
         model = Ingredient
+
+
+class CustomModelSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return self.Meta.model.objects.create(**validated_data)
