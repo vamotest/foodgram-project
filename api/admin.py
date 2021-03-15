eb241762f548f3ab9ba0001c42a70ddbab767afd
vamotest <vamotest@gmail.com>
2021-03-15 20:17:54 +0300

    Add FavoriteAdmin

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Subscription
+from .models import Favorite, Subscription
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'author')
     autocomplete_fields = ('user', 'author')
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    autocomplete_fields = ('user', 'recipe')
