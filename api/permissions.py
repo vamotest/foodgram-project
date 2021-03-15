5a81a2a4d3e8f8e99a518d09d9394a02d0cd0a53
Valery Moiseev <vamotest@gmail.com>
2021-03-15 22:04:24 +0300

    Revert "Review №1"

--- api/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
-
-
-class IsOwnerOrAdmin(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS or request.user.is_superuser:
-            return True
-        return request.user == obj.user
--- recipes/context_processors.py
+++
@@ -1,4 +0,0 @@
-def shop_list_size(request):
-    if not request.user.is_authenticated:
-        return {'shop_list_size': 0}
-    return {'shop_list_size': request.user.purchases.all().count()}
