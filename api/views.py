b3eb94bfc713c9bb2675ae7e8fc07bc3cd5b4697
vamotest <vamotest@gmail.com>
2021-03-18 16:22:07 +0300

    Use google imports style guide

--- api/views.py
+++ api/views.py
@@ -3,11 +3,13 @@ from rest_framework import filters, mixins, status, viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
+from api.models import Favorite
+from api.models import Subscription
+from api.serializers import FavoriteSerializer
+from api.serializers import IngredientSerializer
+from api.serializers import PurchaseSerializer
+from api.serializers import SubscriptionSerializer
 from recipes.models import Ingredient
-
-from .models import Favorite, Subscription
-from .serializers import (FavoriteSerializer, IngredientSerializer,
-                          PurchaseSerializer, SubscriptionSerializer)
 
 
 class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -20,30 +22,33 @@ class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 class CreateDestroyViewSet(mixins.CreateModelMixin,
                            mixins.DestroyModelMixin,
                            viewsets.GenericViewSet):
-
+    
     def get_object(self, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-
+        
         lookup_url = self.lookup_url_kwarg or self.lookup_field
         kwargs_filter = {
             self.lookup_field:
                 self.kwargs[lookup_url], **kwargs
         }
-
+        
         obj = get_object_or_404(queryset, **kwargs_filter)
         self.check_object_permissions(self.request, obj)
         return obj
-
+    
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object(user=self.request.user)
         success = instance.delete()
+        
         context = {'success': bool(success)}
+        
         return Response(context, status=status.HTTP_200_OK)
 
 
 class SubscriptionViewSet(CreateDestroyViewSet):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
+    
     permission_classes = [IsAuthenticated]
     lookup_field = 'author'
 
@@ -51,14 +56,16 @@ class SubscriptionViewSet(CreateDestroyViewSet):
 class FavoriteViewSet(CreateDestroyViewSet):
     queryset = Favorite.objects.all()
     serializer_class = FavoriteSerializer
+    
     permission_classes = [AllowAny]
     lookup_field = 'recipe'
 
 
 class PurchaseViewSet(mixins.ListModelMixin, CreateDestroyViewSet):
     serializer_class = PurchaseSerializer
+    
     permission_classes = [IsAuthenticated]
     lookup_field = 'recipe'
-
+    
     def get_queryset(self):
         return self.request.user.purchases.all()
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,38 +1,45 @@
 from django.urls import include, path
 
-from . import views
+from recipes.views import recipe_new
+from recipes.views import recipe_edit
+from recipes.views import recipe_delete
+from recipes.views import recipe_view_slug
+from recipes.views import recipe_view_redirect
+from recipes.views import purchases
+from recipes.views import purchases_download
+from recipes.views import index
+from recipes.views import subscriptions
+from recipes.views import favorites
+from recipes.views import profile_view
 
 recipes_urls = [
-    path('new/', views.recipe_new, name='recipe_new'),
-    path(
-        '<int:recipe_id>/<slug:slug>/edit/',
-        views.recipe_edit, name='recipe_edit'
-    ),
+    path('new/', recipe_new, name='recipe_new'),
+    path('<int:recipe_id>/<slug:slug>/edit/', recipe_edit, name='recipe_edit'),
     path(
         '<int:recipe_id>/<slug:slug>/delete/',
-        views.recipe_delete, name='recipe_delete'
+        recipe_delete, name='recipe_delete'
     ),
     path(
         '<int:recipe_id>/<slug:slug>/',
-        views.recipe_view_slug, name='recipe_view_slug'
+        recipe_view_slug, name='recipe_view_slug'
     ),
     path(
-        '<int:recipe_id>/', views.recipe_view_redirect,
+        '<int:recipe_id>/', recipe_view_redirect,
         name='recipe_view_redirect'
     ),
 ]
 
 purchases_urls = [
-    path('', views.purchases, name='purchases'),
-    path('download/', views.purchases_download, name='purchases_download'),
+    path('', purchases, name='purchases'),
+    path('download/', purchases_download, name='purchases_download'),
 ]
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('subscriptions/', views.subscriptions, name='subscriptions'),
-    path('favorites/', views.favorites, name='favorites'),
+    path('', index, name='index'),
+    path('subscriptions/', subscriptions, name='subscriptions'),
+    path('favorites/', favorites, name='favorites'),
     path('purchases/', include(purchases_urls)),
     path('recipe/', include(recipes_urls)),
-    path('<str:username>/', views.profile_view, name='profile_view'),
+    path('<str:username>/', profile_view, name='profile_view'),
 ]
